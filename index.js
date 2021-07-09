09f29b90fddf11118084cd4bfcbd440cfa278c81
Ben Little <ben.little@benlittle.dev>
2021-07-09 10:58:32 -0400

    add basic form

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index');
+});
+
+app.post('/submit', (req, res) => {
+	res.render('result', {
+		username: req.body.username,
+		projects: [
+			'test',
+		]
+	});
 });
 
 app.listen(8080, () => { console.log('listening on 8080') });
